e6ca252e596343e2205b7281c287af518d774e64
Estebanmual <esteban.munozal@gmail.com>
2022-03-21 14:49:54 -0600

    On branch feature/implement-css-in-react 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/components/Header.jsx 	new file:   src/components/InputTodo.jsx 	new file:   src/components/Navbar.jsx 	new file:   src/components/TodoContainer.jsx 	new file:   src/components/TodoItem.jsx 	new file:   src/components/TodoItem.module.css 	new file:   src/components/TodosList.jsx 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js
    	Added the basic structure of the app. Added handling events to make work the app. Implement CSS. Implement function to edit task.

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './TodoItem.module.css';
+
+class TodoItem extends React.PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      editing: false,
+    };
+  }
+
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    });
+  }
+
+  handleUpdatedDone = (e) => {
+    if (e.key === 'Enter') {
+      this.setState({ editing: false });
+    }
+  }
+
+  render() {
+    const {
+      todo, handleChangeProps, deleteTodoProps, setUpdate,
+    } = this.props;
+    const completedStyle = {
+      fontStyle: 'italic',
+      color: '#595959',
+      opacity: 0.4,
+      textDecoration: 'line-through',
+    };
+    const viewMode = {};
+    const editMode = {};
+    const { editing } = this.state;
+
+    if (editing) {
+      viewMode.display = 'none';
+    } else {
+      editMode.display = 'none';
+    }
+    return (
+      <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            checked={todo.completed}
+            onChange={() => handleChangeProps(todo.id)}
+          />
+          <span style={todo.completed ? completedStyle : null}>
+            {todo.title}
+          </span>
+          <button type="button" onClick={() => deleteTodoProps(todo.id)}>
+            Delete
+          </button>
+        </div>
+        <input
+          type="text"
+          className={styles.textInput}
+          style={editMode}
+          value={todo.title}
+          onChange={(e) => {
+            setUpdate(e.target.value, todo.id);
+          }}
+          onKeyDown={this.handleUpdatedDone}
+        />
+      </li>
+    );
+  }
+}
+
+TodoItem.defaultProps = {
+  todo: [],
+};
+
+TodoItem.propTypes = {
+  todo: PropTypes.shape({
+    completed: PropTypes.bool,
+    id: PropTypes.string,
+    title: PropTypes.string,
+  }),
+  handleChangeProps: PropTypes.func.isRequired,
+  deleteTodoProps: PropTypes.func.isRequired,
+  setUpdate: PropTypes.func.isRequired,
+};
+
+export default TodoItem;
